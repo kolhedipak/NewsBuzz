fbd2b655a69a311438e72f4c859377d812f12640
IamDk <dipakkolhe148@gmail.com>
2024-06-23 17:00:25 +0530

    newsbuzz app first version

---
+++ src/components/NewsItem.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge bg-danger" style={{background:"#febeb0 !important"}}> {source} </span>
+                    </div>
+                    <div>
+                        <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top img-height" alt="..."/>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title" style={{fontFamily:"fantasy"}}>{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark" style={{border:"3d5a5b"}}>Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
+}
+
+export default NewsItem
